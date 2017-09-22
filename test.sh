9dcb96c069e68fbc21fcbf1d56411c6f15b9d4e4
Rachit Shrivastava <rshriva@ncsu.edu>
2017-09-21 21:21:18 -0400

    Updated tests

--- test.sh
+++ test.sh
@@ -15,9 +15,9 @@ sudo make validate
 cd ..
 sudo insmod kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
-./benchmark/benchmark 8 8192 4
+./benchmark/benchmark 256 8192 4
 cat *.log > trace
 sort -n -k 3 trace > sorted_trace
-./benchmark/validate 8 8192 < sorted_trace
+./benchmark/validate 256 8192 < sorted_trace
 rm -f *.log
 sudo rmmod npheap
